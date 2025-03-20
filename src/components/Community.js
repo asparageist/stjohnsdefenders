f0f8c94a13eaaa17a300e863e2ffac8fc79214d7
Joseph Murray <70991796+asparageist@users.noreply.github.com>
2025-03-20 00:41:13 -0700

    remove Dan's visual elements

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Community.css';
-import danImage1 from '../img/Dan1.jpg';
-import danImage2 from '../img/Dan2.jpg';
 
 const Community = () => {
-  const [isHovered, setIsHovered] = useState(false);
-
   return (
     <div className="hero" id="home">
       <div className="hero-content">
-        <h1>Protecting St. Johns' Community Center </h1>
+        <h1>Protecting St. Johns' Community Center</h1>
         <p>Standing together against budget cuts that affect our community</p>
-        <img 
-          src={isHovered ? danImage2 : danImage1} 
-          alt="Dan" 
-          className="dan-image"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        />
       </div>
     </div>
   );
